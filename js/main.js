7e4b561a8234c9ff03eca35dd15fb0ef72dc1760
Marieglahn <marieglahn@hotmail.com>
2015-04-22 23:22:31 +0200

    got color switching working. needs clean up in code, too much duplicate code

--- js/main.js
+++ js/main.js
@@ -1,74 +1,127 @@
 $(document).ready(function() {
-    $("body").keyup(function(e){
-       if(e.keyCode == 32){
-           // user has pressed space
-           var color = getRandomColor();           
-           document.location.hash = color;
-           $("#switcher").css("display", "inline-block");
-       }
-    });
-    
     var getRandomColor = function(){
-        var randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+        var randomColor = '#'+ ('000000' + (Math.random()*0xFFFFFF<<0).toString(16)).slice(-6);
         return randomColor;
     }
     var isVaildHex = function(color){
          var isOk  = /(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i.test(color);
-         console.log(isOk);
+        console.log(color +' '+'ok?: '+ isOk);
         return isOk;
     }
+    
+    var setBg = function(color){
+        $("body").css("background-color", color);
+    }
+    
+    var setMode = function(mode){
+        $("body").data("mode", mode);
+    }
+    
+    
+    var setAndShowField = function(){
+    
+    }
+    
     var changeColor = function(){
-        var color = document.location.hash;  
-        console.log(color);
-        var valid = isVaildHex(color);
+        var color = document.location.hash,
+            valid = isVaildHex(color);
         if(color !== "" && valid){
-            $("#rgb-colour-field").css("display", "none");
-            $("#colour-field").css("display", "inline-block");
-            $("body").css("background-color", color);
-            $("#colour-field").text(color);
+            var mode = $("body").data("mode");
+            if(mode === "hex"){
+                setBg(color);
+                showHex(color);
+            }else{
+                setBg(color);
+                switchToRgb(color);
+            }
         }
     }
     
-    window.onhashchange = function(){
-        changeColor();
-    }
-    var switchToRgb = function(color){
-        $("#colour-field").css("display", "none");
-        $("#rgb-colour-field").css("display", "inline-block");
-        $("#rgb-colour-field").text("rgb(255,255,255)");
+    var showHex = function(color){
+        $("#rgb-colour-field").css("display", "none");
+        $("#colour-field").css("display", "inline-block");
+        setBg(color);
+        $("#colour-field").text(color);
+        setMode("hex");
+        //$("body").data("mode", "hex");
+        $("#mode").text("To rgb");
 
     }
     
+    //converting a hex color to rgb and returning a string like: 'rgb(23,24,25)'
+    var convertHexToRgb = function(hexcolor){
+        //See http://jsfiddle.net/mantisimo/qNp8C/
+        var h=hexcolor.replace('#', '');
+        h =  h.match(new RegExp('(.{'+h.length/3+'})', 'g'));
+        for(var i=0; i<h.length; i++){
+            h[i] = parseInt(h[i].length==1? h[i]+h[i]:h[i], 16);
+        }
+        return 'rgb('+h.join(',')+')';
+    }
+    
+    //When switching from hex to rgb
+    var switchToRgb = function(hexcolor){
+        $("#colour-field").css("display", "none");
+        var rgbColor = convertHexToRgb(hexcolor);
+        $("#rgb-colour-field").text(rgbColor);
+        $("#rgb-colour-field").show(); 
+        setMode("rgb");
+
+        $("#mode").text("To hex");
+    }
+    
+    //When switching from rgb to hex without changing the color
     var switchToHex = function(){
         var color = document.location.hash;  
         var valid = isVaildHex(color);
         if(color !== "" && valid){
-                        $("#rgb-colour-field").css("display", "none");
-            $("#colour-field").css("display", "inline-block");
+            $("#rgb-colour-field").css("display", "none");
+            $("#colour-field").show();
             $("#colour-field").text(color);
+            //$("body").data("mode", "hex");
+            setMode("hex");
+
+            $("#mode").text("To rgb");
         }
     }
     
-    $("#switcher").click(function(){
+    //Calling this function when switcher is pressed
+    var switchMode = function(){
         var mode = $("body").data("mode");
         if(mode === "hex"){
-            $("body").data("mode", "rgb");
             var color = $("#colour-field").text();
-            $("#mode").text("hex");
             switchToRgb(color);
         } else {
-            $("body").data("mode", "hex");
-            $("#mode").text("rgb");
             switchToHex();
         }
+    }
+    
+    //When hash change (new color is written in the hash)
+    window.onhashchange = function(){
+        changeColor();
+    }
+    
+    //If space is pressed:
+    $("body").keyup(function(e){
+       if(e.keyCode == 32){
+           // user has pressed space
+           var color = getRandomColor(); 
+           //Writes in the random color in the hash:
+           document.location.hash = color;
+       }
     });
     
-    $("#info").click(function(){
-//        if($("#contact").is(':visible')){
-            $("#contact").toggle("fast");
-//        }else {
-//            $("#contact").show('fast');
-//        }
+    //Clicking btn to switch from hex to rgb or vice versa
+    $("#switcher").click(function(){
+        //console.log('switch');
+        switchMode();
     });
+    
+    //View page info:
+    $("#info").click(function(){
+        $("#contact").toggle("fast");
+    });
+    
     changeColor();
+    //switchMode();
 });
